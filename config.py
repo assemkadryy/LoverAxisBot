18e175e09aed1be26b28949106fd5c317efa33a5
Assem K. Elsherif <assemkadry23@gmail.com>
2026-03-29 18:50:04 +0100

    Add Stripe and Telegram bot configuration variables

--- config.py
+++ config.py
@@ -6,21 +6,27 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_GROUP_ID = int(os.getenv("TELEGRAM_GROUP_ID", "0"))
 ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", "0"))
+TELEGRAM_BOT_USERNAME = os.getenv("TELEGRAM_BOT_USERNAME", "")
 
-# Stripe fixed payment links
-STRIPE_MONTHLY_LINK = os.getenv(
-    "STRIPE_MONTHLY_LINK",
-    "https://buy.stripe.com/9B68wP6e39dk7Hg4cndnW02",
-)
-STRIPE_BIWEEKLY_LINK = os.getenv(
-    "STRIPE_BIWEEKLY_LINK",
-    "https://buy.stripe.com/fZu14n6e3cpw0eOgZ9dnW03",
-)
+# Stripe
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
+STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET", "")
 
+# Display prices
 CURRENCY_CODE = os.getenv("CURRENCY_CODE", "USD")
 MONTHLY_PRICE = int(os.getenv("MONTHLY_PRICE", "230"))
 BIWEEKLY_PRICE = int(os.getenv("BIWEEKLY_PRICE", "140"))
 
+# Stripe amounts in cents (230 USD = 23000, 140 USD = 14000)
+MONTHLY_AMOUNT = int(os.getenv("MONTHLY_AMOUNT", "23000"))
+BIWEEKLY_AMOUNT = int(os.getenv("BIWEEKLY_AMOUNT", "14000"))
+
 # Subscription durations in days
 MONTHLY_DAYS = 30
 BIWEEKLY_DAYS = 14
+
+# Public URL of this server (Railway domain)
+PUBLIC_URL = os.getenv("PUBLIC_URL", "")
+
+# Webhook server port (Railway sets PORT automatically)
+PORT = int(os.getenv("PORT", "8080"))
